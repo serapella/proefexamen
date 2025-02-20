df3854267604a246d7653936b7625b65b603574f
Serap Cakmak <serap.cakmak@hotmail.com>
2025-02-20 14:23:19 +0100

    envirmnt variablenot live

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use("/drivers", driverRoutes);
 app.use("/circuits", circuitRoutes);
 
 mongoose
-  .connect(process.env.MONGODB_URI_LIVE as string)
+  .connect(process.env.MONGODB_URI as string)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
